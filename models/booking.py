a82c22dc94fafc63b258e1483e0d33b5730496e2
Aneeqa Yusaf <aneeqayusaf@outlook.com>
2023-03-26 17:24:13 +0100

    set up html pages

--- models/booking.py
+++ models/booking.py
@@ -1,6 +1,7 @@
 class Booking:
     
-    def __init__(self, date, time, id=None):
+    def __init__(self, date, time, treatment=[], id=None):
         self.date = date
         self.time = time
+        self.treatment = treatment
         self.id = id
