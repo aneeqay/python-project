f83843930d538e6a6ae6f3dee6527180785116b9
Aneeqa Yusaf <aneeqayusaf@outlook.com>
2023-03-26 16:33:56 +0100

    created classes

--- models/booking.py
+++ models/booking.py
@@ -1,2 +1,7 @@
 class Booking:
-    pass
+    
+    def __init__(self, date, time, treatment_list=None, id=None):
+        self.date = date
+        self.time = time
+        self.treatment_list = treatment_list
+        self.id = id
--- models/treatment.py
+++ models/treatment.py
@@ -1,2 +1,7 @@
 class Treatment:
-    pass
+    
+    def __init__(self, name, duration, description, id=None):
+        self.name = name
+        self.duration = duration
+        self.description = description
+        self.id = id
